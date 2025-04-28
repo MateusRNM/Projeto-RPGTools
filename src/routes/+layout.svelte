97c016f07d879268444d64c513b4744b238f13ea
MateusRNM <mateusmt676@gmail.com>
2025-04-27 21:36:00 -0400

    Adição do arquivo da fonte no projeto de forma local.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    let { children } = $props();
+</script>
+
+<main>{@render children()}</main>
+
+<style>
+@font-face {
+    font-family: 'imp';
+    src: url('../lib/assets/impact.ttf');
+}
+* {
+    font-family: 'imp';
+    color: rgb(214, 212, 212);
+}
+</style>
--- src/routes/mainMenu/+page.svelte
+++ src/routes/mainMenu/+page.svelte
@@ -361,8 +361,12 @@
 </div>
 
 <style>
+@font-face {
+    font-family: 'imp';
+    src: url('../lib/assets/impact.ttf');
+}
 * {
-    font-family: Impact;
+    font-family: 'imp';
     color: rgb(214, 212, 212);
 }
 :global(body){
